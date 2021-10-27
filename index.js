94a9907cd66fffed00563498024cd0d842297d98
ZafarIqbal777 <zafariqbal.itnets@gmail.com>
2021-10-27 14:55:16 +0500

    final1  attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'final attempt!\n'
+  const msg = 'final attempt, delete deploy script from code!\n'
   res.end(msg);
 });
 
