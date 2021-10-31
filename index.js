8a057861f6c2b674f8a473f48bb1a4ce32ebf6f6
ZafarIqbal777 <zafariqbal.itnets@gmail.com>
2021-10-31 23:14:43 +0500

    thanks its finally dun

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'its my latest and final update \n'
+  const msg = 'Finally ddeployed at 11:30 at night  \n'
   res.end(msg);
 });
 
