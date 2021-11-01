b4727f8ff1b6ecabec5a83abf68c7ae3465e57e8
ZafarIqbal777 <zafariqbal.itnets@gmail.com>
2021-11-01 12:51:29 +0500

    html and node app deployment

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+var http = require('http');
 var fs = require('fs');
-
-const server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+}).listen(3000);
